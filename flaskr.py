5bf7a4c303154ac04c922521004eaacc154bb338
moonoroman <1049726823@qq.com>
2016-05-27 10:55:03 +0800

    fix

--- flaskr.py
+++ flaskr.py
@@ -1,43 +1,56 @@
 # all import
 import os
-import sqlite3
+from sqlite3 import dbapi2 as sqlite3
 from flask import Flask,request,session,g,redirect,url_for,abort,render_template,flash
 from contextlib import closing
 
-# configuration
-DEBUG = True
-SECRET_KEY = 'development key'
-USERNAME = 'admin'
-PASSWORD = 'default'
-
 #create our little application
 app = Flask(__name__)
-app.config.from_object(__name__) 
+
+# Load default config and override config from an environment variable
+app.config.update(dict(DATABASE = os.path.join(app.root_path,'flaskr.db'),
+DEBUG = True,
+SECRET_KEY = 'development key',
+USERNAME = 'admin',
+PASSWORD = 'default')
+)
+app.config.from_envvar('FLASKR_SETTING', silent=True)
 
 #connect to the specific database
 def connect_db():
-	return sqlite3.connect('database/flaskr.db')
+	rv = sqlite3.connect(app.config['DATABASE'])
+	rv.row_factory = sqlite3.Row
+	return rv
 
-#initialize the database
+def get_db():
+	"""Opens a new database connection if there is none yet for the
+    current application context.
+    """
+	if not hasattr(g,'sqlite_db'):
+		g.sqlite_db = connect_db()
+	return g.sqlite_db
+
 def init_db():
-	with closing(connect_db()) as db:
-		with app.open_resource('schema.sql') as f:
-			db.cursor().executescript(f.read())
-		db.commit()
+	db = get_db()
+	with app.open_resource('schema.sql',mode='r') as f:
+		db.cursor().executescript(f.read())
+	db.commit()
 
-@app.before_request
-def before_request():
-	g.db = connect_db()
+@app.cli.command('initdb')
+def initdb():
+	'''Create the database tables.'''
+	init_db()
+	print('Initialize the database')
 
-@app.teardown_request
-def teardown_request(exception):
-	db = getattr(g,'db',None)
-	if db is not None:
-		db.close()
+@app.teardown_appcontext
+def close_db(error):
+	if hasattr(g,'sqlite_db'):
+		g.sqlite_db.close()
 				
 @app.route('/')
 def show_entries():
-	cur = g.db.execute('select title, text from entries order by id desc')
+	db = get_db()
+	cur = db.execute('select title, text from entries order by id desc')
 	entries = [dict(title=row[0],text=row[1]) for row in cur.fetchall()]
 	return render_template('show_entries.html', entries=entries)
 
@@ -45,8 +58,9 @@ def show_entries():
 def add_entries():
 	if not session.get('logged_in'):
 		abort(401)
-	g.db.execute('insert into entries (title, text) values (?, ?)',[request.form['title'],request.form['text']])
-	g.db.commit()
+	db=get_db()
+	db.execute('insert into entries (title, text) values (?, ?)',[request.form['title'],request.form['text']])
+	db.commit()
 	flash('New entry was successfully posted')
 	return redirect(url_for('show_entries'))
 
@@ -69,7 +83,3 @@ def logout():
 	session.pop('logged_in',None)
 	flash('You were logged out')
 	return redirect(url_for('show_entries'))
-
-if __name__ == '__main__':
-	app.run()
-	
